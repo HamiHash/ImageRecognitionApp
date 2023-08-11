1c6216a4979db61306cc95c31ff198e6cd960f84
hamihash <hami.hash@yahoo.com>
2023-08-12 01:24:52 +0330

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Hamed Hashemi on 8/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
