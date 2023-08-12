623793f6c273d5b165cbd23d0d0742e4226daccb
hamihash <hami.hash@yahoo.com>
2023-08-13 02:05:54 +0330

    finish

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -1,13 +1,13 @@
 //
 //  ViewController.swift
-//  SeaFood
+//  objectRecogniser
 //
 //  Created by Hamed Hashemi on 8/12/23.
 //
 
 import UIKit
 import CoreML
-import Vision
+import Vision // VNCoreMLModel
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -20,7 +20,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
-        imagePicker.allowsEditing = false // later we can make it true to exand the app
+        imagePicker.allowsEditing = false // later we can make it true to expand the app
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -29,17 +29,50 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             // add it to the imageView
             imageView.image = userPickedImage
+            
+            // changing the type to CIImage which is for CoreML specific type
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
     }
+    
+    // this will process the image
+    func detect(image: CIImage) {
+        // Creating an object called model, using the VNcoreMLModel and loading the Inceptionv3 model as the model
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("loading core ml model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("model failed to process image, failed to downcast results")
+            }
+            
+            if let firstResult = results.first?.identifier {
+                self.navigationItem.title = firstResult
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print("error while perform: \(error)")
+        }
+    }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         // present the image picker to the user
         present(imagePicker, animated: true, completion: nil)
-        
-        
     }
+    
 }
 
